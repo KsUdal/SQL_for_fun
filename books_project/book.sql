10877979c1b15ae9155f39532001de09b762c341
Ksenia <70650376+KsUdal@users.noreply.github.com>
2025-05-05 21:18:58 +0300

    Update book.sql
    New tables added

--- books_project/book.sql
+++ books_project/book.sql
@@ -195,3 +195,135 @@ WHERE ABS(amount - (SELECT MAX(amount) FROM book)) >0;
 SELECT SUM(price)
 FROM book
 WHERE price = ANY(SELECT MIN(price) FROM book GROUP BY author);
+
+---------------------
+-- добавление новой таблицы
+DROP TABLE IF EXISTS supply;
+CREATE SEQUENCE supply_id_seq;
+CREATE TABLE supply(
+    supply_id INT DEFAULT nextval('supply_id_seq'),
+    title VARCHAR(50),
+    author VARCHAR(30),
+    price DECIMAL(8, 2),
+    amount INT
+);
+ALTER SEQUENCE supply_id_seq OWNED BY supply.supply_id;
+/*AUTO_INCREMENT doesn't work in PostgreSQL*/
+
+INSERT INTO supply(title, author, price, amount)
+VALUES
+    ('Лирика', 'Пастернак Б.Л.', 518.99, 2),
+    ('Черный человек', 'Есенин С.А.', 570.20, 6),
+    ('Белая гвардия', 'Булгаков М.А.', 540.50, 7),
+    ('Идиот', 'Достоевский Ф.М.', 360.80, 3);
+
+/*Добавить из таблицы supply в таблицу book, все книги, 
+кроме книг, написанных Булгаковым М.А. и Достоевским Ф.М.*/
+INSERT INTO book (title, author, price, amount) 
+SELECT title, author, price, amount 
+FROM supply
+WHERE author NOT IN ('Булгаков М.А.', 'Достоевский Ф.М.');
+SELECT * FROM book;
+
+--Занести из таблицы supply в таблицу book только те книги, авторов которых нет в  book.
+INSERT INTO book (title, author, price, amount) 
+SELECT title, author, price, amount 
+FROM supply
+WHERE author NOT IN (
+        SELECT author
+        FROM book
+      );
+SELECT * FROM book;
+
+/*Уменьшить на 10% цену тех книг в таблице book, 
+количество которых принадлежит интервалу от 5 до 10, включая границы.*/
+UPDATE book 
+SET price = 0.9 * price
+WHERE amount BETWEEN 5 AND 10;
+SELECT * FROM book ;
+
+/* ! buy column has to be added ! */
+
+/*В таблице book необходимо скорректировать значение для покупателя в столбце buy таким образом, 
+чтобы оно не превышало количество экземпляров книг, указанных в столбце amount. 
+А цену тех книг, которые покупатель не заказывал, снизить на 10%.*/
+UPDATE book 
+SET buy = amount
+WHERE buy > amount;
+UPDATE book
+SET price = 0.9*price
+WHERE buy = 0;
+SELECT * FROM book;
+
+/*Для тех книг в таблице book , которые есть в таблице supply, не только увеличить их количество 
+в таблице book ( увеличить их количество на значение столбца amountтаблицы supply), 
+но и пересчитать их цену (для каждой книги найти сумму цен из таблиц book и supply и разделить на 2).*/
+UPDATE book, supply 
+SET book.amount = book.amount + supply.amount,
+    book.price = (book.price + supply.price)/2
+WHERE book.title = supply.title AND book.author = supply.author;
+SELECT * FROM book;
+
+/*Удалить из таблицы supply книги тех авторов, 
+общее количество экземпляров книг которых в таблице book превышает 10.*/
+DELETE FROM supply 
+WHERE author IN (
+        SELECT author 
+        FROM book
+        GROUP BY author
+        HAVING SUM(amount) > 10
+      );
+SELECT * FROM supply;
+
+
+/*Создать таблицу заказ (ordering), куда включить авторов и названия тех книг, количество экземпляров 
+которых в таблице book меньше среднего количества экземпляров книг в таблице book. 
+В таблицу включить столбец   amount, в котором для всех книг 
+указать одинаковое значение - среднее количество экземпляров книг в таблице book.*/
+CREATE TABLE ordering AS
+SELECT author, title, (
+    SELECT ROUND(AVG(amount),0)
+    FROM book) AS amount
+FROM book
+WHERE amount < (SELECT AVG(amount) FROM book);
+SELECT * FROM ordering;
+
+/*В стране Х введена цензура - все книги авторства Достоевского нужно изъять, более 10 книг 
+одного экземпляра на складе хранить нельзя (кроме Кодекса хорошего гражданина), а книги, 
+в названии или авторе которых есть буква "н" нужно удорожить на 100%! 
+При этом по одному экземпляру изымаемых книг Достоевского хочет оставить себе букинист-директор 
+на память, а все изъятые книги нужно заменить тем же количеством книг 
+"Кодекс хорошего гражданина" авторства Министерство патриотизма, чтобы на полках не было пробелов. 
+- Обновить список наличия и цену книг в book;
+- Сделать для директора список книг archive, которые он заберёт;
+- Исправить список на закупку supply так, 
+чтобы купить Кодексы в правильном количестве и не купить запрещёнку.*/
+CREATE TABLE archive AS 
+SELECT author, title 
+FROM book 
+WHERE author = 'Достоевский Ф.М.';
+SELECT * FROM archive;
+
+UPDATE supply 
+SET title = 'Кодекс хорошего гражданина', 
+  author = 'Министерство патриотизма', 
+  price = 146.00, 
+  amount = (
+    SELECT SUM(amount) 
+    FROM book 
+    WHERE author = 'Достоевский Ф.М.' OR amount > 10
+  ) 
+WHERE author = 'Достоевский Ф.М.';
+SELECT * FROM supply;
+
+DELETE FROM book 
+WHERE author = 'Достоевский Ф.М.';
+
+UPDATE book 
+SET amount = 10 
+WHERE amount > 10 AND author <> 'Министерство патриотизма';
+
+UPDATE book 
+SET price = price * 2 
+WHERE title LIKE "%н%" OR author LIKE "%н%";
+SELECT * FROM book;
